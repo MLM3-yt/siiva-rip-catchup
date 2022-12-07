8d8cb9b6fbc3748ac4adc414bbef65ee10db231f
MLM3 <ttcraft800@gmail.com>
2022-12-07 11:55:15 +0100

    gitignore update

---
+++ ripcatchup.js
@@ -0,0 +1,9 @@
+// Template URL for temporary playlists: https://www.youtube.com/watch_videos?video_ids=[video ID 1],[video ID 2]
+
+
+// apikey = readFile("apikey.txt")
+apikey = 2
+
+function getAPIKey() {
+    return this.apikey
+}
