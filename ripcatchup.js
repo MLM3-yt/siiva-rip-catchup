2cf41bf0f858f68892a560fc802c9fb30c58e682
MLM3 <ttcraft800@gmail.com>
2023-01-19 00:03:43 +0100

    blblbl

--- ripcatchup.js
+++ ripcatchup.js
@@ -1,9 +1,5 @@
 // Template URL for temporary playlists: https://www.youtube.com/watch_videos?video_ids=[video ID 1],[video ID 2]
 
 
-// apikey = readFile("apikey.txt")
-apikey = 2
 
-function getAPIKey() {
-    return this.apikey
-}
+document.getElementById("demo").innerHTML = 3;
